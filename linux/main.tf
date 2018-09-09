47d2a70f133736aad49b93fc5d14c234a9bae93a
Obinna Okpokwasili <limitless@obinnas-mbp-2.lan>
2018-09-09 14:15:37 -0500

    added aws provider, and created main.tf

--- linux/main.tf
+++ linux/main.tf
@@ -2,10 +2,6 @@
 # Configuration and Deployment of AWS Instance for Linux
 #
 ####
-provider "aws" {
-  region = "${var.region}"
-}
-
 resource "aws_network_interface" "linux" {
   subnet_id       = "${var.subnet_id}"
   security_groups = ["${var.security_groups}"]
